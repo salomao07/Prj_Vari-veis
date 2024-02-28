4736496ef71f46858143ae9b5a96ed1cc8303990
salomao07 <133671216+salomao07@users.noreply.github.com>
2024-02-28 12:15:09 -0300

    Add files via upload

---
+++ Prj_variaveis/src/Prj_variaveis.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : Prj_variaveis.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+int main(void) {
+	int idade = 26;
+	const int MAX_NUM = 100;
+
+	printf("Minha variável int comum = %d\n\n", idade);
+	printf("Minha variável int constante = %d\n\n", MAX_NUM);
+	printf("Minha constante = %f\n", PI);
+
+	//MAX_NUM = 99;)
+
+	return EXIT_SUCCESS;
+}
